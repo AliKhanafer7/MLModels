1d21980c003204c4a6f8ad422d22b4b99c07d97c
alikhanafer7 <ali.h.khanafer@gmail.com>
2020-08-16 11:44:58 -0400

    KMeans scaffolding

---
+++ clustering/cluster/KMeans.py
@@ -0,0 +1,22 @@
+class KMeans :
+    coef_ = np.array([]) # Estimated coefficients
+    def fit(self, X, y):
+        pass
+
+    def get_params(self):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, predictions, y, sample_weights = None):
+        pass
+
+    def msee(self, actual, predicted):
+        pass
+
+    def add_bias(self,X):
+        pass
+   
+    def remove_categorical_cols(self, df):
+        pass
